61eda184d9cbcdc69154992e97f47114b86198fe
Timdpr <trussell@amplifylife.com>
2019-11-09 10:20:36 +0000

    Optimisations

--- src/hashkeys.cpp
+++ src/hashkeys.cpp
@@ -4,13 +4,12 @@
 U64 generatePositionKey(const Board *board) { // returns rather than sets key
 
     U64 finalKey = 0;
-    int piece;
 
     // Loop through squares on board
     for (int square = 0; square < BRD_SQ_NUM; ++square) {
-        piece = board->pieces[square]; // set piece to whatever is in board's pieces array at this square
+        int piece = board->pieces[square]; // set piece to whatever is in board's pieces array at this square
         if (piece != NO_SQ && piece != OFFBOARD && piece != EMPTY) { // if piece is not a border square AND not empty...
-            ASSERT(piece >= wP && piece <= bK); // ...then it must be a real piece (white pawn up to black king)
+            ASSERT(piece >= wP && piece <= bK) // ...then it must be a real piece (white pawn up to black king)
             finalKey ^= pieceKeys[piece][square]; // hash in whatever number is stored at piece keys array here
         }
     }
@@ -23,12 +22,12 @@ U64 generatePositionKey(const Board *board) { // returns rather than sets key
     // en passant: if pawn has moved 2 squares in prev. move, en passant square is set
     // If en passant square is set, and within bounds, then hash in
     if (board->enPasSq != NO_SQ) {
-        ASSERT(board->enPasSq >= 0 && board->enPasSq < BRD_SQ_NUM);
+        ASSERT(board->enPasSq >= 0 && board->enPasSq < BRD_SQ_NUM)
         finalKey ^= pieceKeys[EMPTY][board->enPasSq];
     }
 
     // Hash in castling permission key
-    ASSERT(board->castlingPerms >= 0 && board->castlingPerms <= 15);
+    ASSERT(board->castlingPerms >= 0 && board->castlingPerms <= 15)
     finalKey ^= castleKeys[board->castlingPerms];
 
     return finalKey;
