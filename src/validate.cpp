1411655427367426667c87f0daa220ecc9819224
Timdpr <trussell@amplifylife.com>
2019-11-09 10:22:42 +0000

    Add validation methods

--- src/validate.cpp
+++ src/validate.cpp
@@ -1,5 +1,21 @@
-//
-// Created by Timothy Russell on 05/11/2019.
-//
+#include "defs.h"
 
-#include "validate.h"
+bool squareOnBoard(const int sq) {
+    return !(filesBoard[sq] == OFFBOARD);
+}
+
+bool sideValid(const int side) {
+    return side == WHITE || side == BLACK;
+}
+
+bool fileOrRankValid(const int fileOrRank) {
+    return fileOrRank >= 0 && fileOrRank <= 7;
+}
+
+bool pieceValid(const int piece) {
+    return piece >= wP && piece <= bK;
+}
+
+bool pieceValidOrEmpty(const int piece) {
+    return piece >= EMPTY && piece <= bK;
+}
