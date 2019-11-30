e53e19b7235c75f27b80fef06cd06ad0297741e9
Timdpr <trussell@amplifylife.com>
2019-11-30 10:31:50 +0000

    Refactor for uci loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,68 +1,17 @@
-#include <cstdio>
-#include <iostream>
 #include "defs.h"
 
 int main() {
-
-    const char *PAWNMOVESW = "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1";
-    const char *PAWNMOVESB = "rnbqkbnr/p1p1p3/3p3p/1p1p4/2P1Pp2/8/PP1P1PpP/RNBQKB1R b KQkq e3 0 1";
-    const char *KNIGHTSKINGS = "5k2/1n6/4n3/6N1/8/3N4/8/5K2 w - - 0 1";
-    const char *ROOKS = "6k1/8/5r2/8/1nR5/5N2/8/6K1 b - - 0 1";
-    const char *QUEENS = "6k1/8/4nq2/8/1nQ5/5N2/1N6/6K1 b - - 0 1 ";
-    const char *BISHOPS = "6k1/1b6/4n3/8/1n4B1/1B3N2/1N6/2b3K1 b - - 0 1 ";
-    const char *CASTLE1 = "r3k2r/8/8/8/8/8/8/R3K2R w KQkq - 0 1";
-    const char *CASTLE2 = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
-    const char *TRICKYFEN = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
-    const char *TRICKYFEN2 = "n1n5/PPPk4/8/8/8/8/4Kppp/5N1N w - - 0 1";
-
+    // TODO: videos 72, 73, 74 AND 75!!
     initAll();
 
-    Board board[1];
-    MoveList moveList[1];
+    Board *board = genBoard();
+    SearchInfo info[1];
+    initPVTable(board->pvTable);
+    info->quit = FALSE;
 
-    parseFen(START_FEN, board);
+    uciLoop(board, info);
 
-    char input[6];
-    int pvNum = 0;
-    int max = 0;
-    printBoard(board);
-    while (true) {
-        printf("\nPlease enter a move, 't' to undo last move, 'p' to run perft test, or 'q' to quit: ");
-        fgets(input, 7, stdin);
-        if (input[4] == '\n') input[4] = '\0';
-        if (input[5] == '\n') input[5] = '\0';
-
-        if (input[0] == 'q') {
-            break;
-        } else if (input[0] == 't') {
-            takeMove(board);
-            printBoard(board);
-        } else if (input[0] == 'p') {
-            perftTest(4, board);
-        } else if (input[0] == 'r') {
-            max = getPVLine(4, board);
-            printf("pvLine of %d Moves: ", max);
-            for (pvNum = 0; pvNum < max; ++pvNum) {
-                int move = board->pvArray[pvNum];
-                printf(" %s", printMove(move));
-            }
-            printf("\n");
-        } else {
-            int move = parseMove(input, board);
-            if (move != NO_MOVE) {
-                storePVMove(board, move);
-                makeMove(board, move);
-                if (isRepetition(board)) {
-                    printf("REPETITION!!\n");
-                }
-                printBoard(board);
-            } else {
-                printf("- Move not parsed: %s\n", input);
-            }
-        }
-
-        fflush(stdin);
-    }
+    free(board->pvTable->pTable);
 
     return 0;
 }
