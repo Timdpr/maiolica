b9fcc3bb57c5a1677852970bce5dfef91fa1941d
Timdpr <trussell@amplifylife.com>
2019-12-03 19:51:50 +0000

    New entrypoints

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,53 @@
+#include <cstring>
 #include "defs.h"
 
 int main() {
-    // TODO: videos 72, 73, 74 AND 75!!
-    //   There are then some extra bits added to xboard in later videos
+    // TODO: see part 87 for next steps...
+    // TODO: Not doing polyglot books for now
     initAll();
 
     Board *board = genBoard();
     SearchInfo info[1];
-    initPVTable(board->pvTable);
     info->quit = FALSE;
+    initHashTable(board->hashTable, 64);
 
-    uciLoop(board, info);
+    setbuf(stdin, nullptr);
+    setbuf(stdout, nullptr);
 
-    free(board->pvTable->pTable);
+    printf("Welcome to Maiolica! Type 'console' for console mode...\n");
+
+    char line[256];
+    while (TRUE) {
+        memset(&line[0], 0, sizeof(line));
+        fflush(stdout);
+
+        if (!fgets(line, 256, stdin))
+            continue;
+
+        if (line[0] == '\n')
+            continue;
+
+        if (!strncmp(line, "uci", 3)) {
+            uciLoop(board, info);
+            if (info->quit == TRUE) break;
+            continue;
+
+        } else if (!strncmp(line, "xboard", 6))	{
+            xBoardLoop(board, info);
+            if (info->quit == TRUE) break;
+            continue;
+
+        } else if (!strncmp(line, "console", 4))	{
+            consoleLoop(board, info);
+            if (info->quit == TRUE) break;
+            continue;
+
+        } else if (!strncmp(line, "quit", 4))	{
+            break;
+        }
+    }
+
+    free(board->hashTable->hTable);
 
     return 0;
 }
